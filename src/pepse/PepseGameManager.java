9ab58c914d569fbd958ab72f02288739e76fe944
yuli pikman <pikman.yuli@huji.mail.ac.il>
2024-12-26 18:30:14 +0200

    Added Sky (I will add clouds next week)

---
+++ src/pepse/PepseGameManager.java
@@ -0,0 +1,54 @@
+package pepse;
+
+import danogl.GameManager;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import danogl.util.Vector2;
+import pepse.world.trees.Sky;
+
+
+public class PepseGameManager extends GameManager {
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener,
+                               WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+
+        Vector2 windowDimensions = windowController.getWindowDimensions();
+
+        gameObjects().addGameObject(Sky.create(windowDimensions), Layer.BACKGROUND);
+
+//        GameObject cloud1 = createCloud(this.windowDimensions, new Vector2(200, 100)); // ענן ראשון
+//        GameObject cloud2 = createCloud(this.windowDimensions, new Vector2(500, 150)); // ענן שני
+//
+//        // הוספת העננים לשכבה
+//        gameObjects().addGameObject(cloud1, Layer.BACKGROUND);
+//        gameObjects().addGameObject(cloud2, Layer.BACKGROUND);
+    }
+
+
+//    private GameObject createCloud(Vector2 windowDimensions, Vector2 position) {
+//        Color cloudColor = Color.decode("#FFFFFF"); // צבע הענן לבן
+//        Vector2 cloudSize = new Vector2(150, 50); // גודל הענן
+//
+//        // יצירת הענן (מלבן לבן)
+//        GameObject cloud = new GameObject(position, cloudSize, new RectangleRenderable(cloudColor));
+//        cloud.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES); // הענן ינוע עם המצלמה
+//        cloud.setTag("cloud"); // הוספת תג עבור הדיבוג
+//
+//        return cloud;
+//    }
+
+    public void run() {
+        super.run();
+    }
+
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+
+}
+
