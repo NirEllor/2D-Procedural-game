8d9a3c63e65893ec0c30a8c56baf91508a858cda
Nir Ellor <nirellorwaizner@gmail.com>
2024-12-27 16:57:08 +0200

    did Block.java

--- src/pepse/world/trees/Block.java
+++ src/pepse/world/trees/Block.java
@@ -1,4 +1,22 @@
 package pepse.world.trees;
 
-public class Block {
+import danogl.GameObject;
+import danogl.components.GameObjectPhysics;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class Block extends GameObject {
+
+    private static final String BLOCK_TAG = "Block";
+    public static final int SIZE = 30;
+
+    public Block(Vector2 topLeftCorner, Renderable renderable) {
+
+        super(topLeftCorner, Vector2.ONES.mult(SIZE), renderable);
+        physics().preventIntersectionsFromDirection(Vector2.ZERO);
+        physics().setMass(GameObjectPhysics.IMMOVABLE_MASS);
+        setTag(BLOCK_TAG);
+
+    }
 }
+
