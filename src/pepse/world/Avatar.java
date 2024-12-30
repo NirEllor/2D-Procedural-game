3da6f23bf695f9d715ee5df6f989f3ebcfe7762b
Nir Ellor <nirellorwaizner@gmail.com>
2024-12-30 13:51:14 +0200

    removed duplicates from assets

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -7,7 +7,7 @@ import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 public class Avatar {
-//    private static final String avatarImagePath = "assets/assets/idle_0.png";
+    private static final String avatarImagePath = "src/assets/__MACOSX/assets/._idle_0.png";
     private Vector2 topLeftCorner;
     private UserInputListener inputListener;
     private ImageReader imageReader;
@@ -17,7 +17,7 @@ public class Avatar {
         this.topLeftCorner = topLeftCorner;
         this.inputListener = inputListener;
         this.imageReader = imageReader;
-        ImageRenderable heartImage = imageReader.readImage(ASSETS_HEART_PNG,true);
+        ImageRenderable heartImage = imageReader.readImage(avatarImagePath,true);
 
     }
 }
